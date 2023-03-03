d22bc80a13745887b1d14d68dd48e0c549f42dae
Denis Wirries <denis@wirries.me>
2023-03-03 11:21:05 +0100

    test

--- src/renderer.js
+++ src/renderer.js
@@ -1,1 +1,9 @@
 // renderer.js
+
+let $ = require('jquery')  // jQuery now loaded and assigned to $
+let count = 0
+$('#click-counter').text(count.toString())
+$('#countbtn').on('click', () => {
+   count++
+   $('#click-counter').text(count)
+})
